7d6061b742dc6d3bc486eb384b9c9b7afccd412b
BenaventeX24 <81106944+BenaventeX24@users.noreply.github.com>
2021-11-21 01:41:38 -0300

    Update admin.sh

--- proyecto/admin.sh
+++ proyecto/admin.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Variable que contiene el grep a la carpeta de usuarios buscando admin
+# Variable que contiene el grep a la carpeta de usuarios buscando tutor
 verify=$(grep administrador /etc/passwd)
 
-# Comprueba si la búsqueda de admin es vacía
+# Comprueba si la búsqueda de tutor es vacía
 if [ -z $verify ]
 then
-    # Crea un administrador y creá su directorio base (/home/adminUser) con '-m'. Además, añade su contraseña como 'admin' por default.
+    # Crea un tutor y creá su directorio base (/home/tutor) con '-m'. Además, añade su contraseña como 'tutor' por default.
     sudo useradd -m administrador
     echo administrador:administrador | sudo chpasswd
 fi
@@ -14,6 +14,6 @@ fi
 # Da permisos al tutor de leer, escribir y ejecutar la carpeta del proyecto y todos sus subdirectorios y archivos
 sudo setfacl -R -m u:administrador:rwx /home/anima/proyecto
 
-# Loguear al usuario tutor y ejecutar el comando 'bash tutorScript.sh'.
+# Loguear al usuario tutor y ejecutar el comando 'bash adminScript.sh'.
 # Debe pedir contraseña de tutor.
 su --login administrador -c "bash /home/anima/proyecto/adminScript.sh"
